f65594c6e3f8522f40f103eca0b3c70f3d5d8b42
OkGameStudios <oskarkottom@gmail.com>
2023-10-01 00:15:27 +0200

    First publish to v0.0.0

--- src-tauri/src/injections/titlebar.js
+++ src-tauri/src/injections/titlebar.js
@@ -1,24 +1,24 @@
-addEventListener("load", () => {
-    addTitleBar()
-  })
+// addEventListener("load", () => {
+//     addTitleBar()
+//   })
   
-  function addTitleBar() {
-    const innerApp = document?.querySelector('div[id="app-mount"]')
+//   function addTitleBar() {
+//     const innerApp = document?.querySelector('div[id="app-mount"]')
   
-    const titlebar = document.createElement("div")
-    titlebar.setAttribute("data-tauri-drag-region", "")
-    titlebar.setAttribute("class", "titlebar")
-    titlebar.innerHTML =`
-  <div class="titlebar-button" id="titlebar-minimize">
-    -
-  </div>
-  <div class="titlebar-button" id="titlebar-maximize">
-    []
-  </div>
-  <div class="titlebar-button" id="titlebar-close">
-    X
-  </div>
-    `
+//     const titlebar = document.createElement("div")
+//     titlebar.setAttribute("data-tauri-drag-region", "")
+//     titlebar.setAttribute("class", "titlebar")
+//     titlebar.innerHTML =`
+//   <div class="titlebar-button" id="titlebar-minimize">
+//     -
+//   </div>
+//   <div class="titlebar-button" id="titlebar-maximize">
+//     []
+//   </div>
+//   <div class="titlebar-button" id="titlebar-close">
+//     X
+//   </div>
+//     `
   
-    innerApp.insertBefore(titlebar, innerApp.firstChild)
-  }
+//     innerApp.insertBefore(titlebar, innerApp.firstChild)
+//   }
